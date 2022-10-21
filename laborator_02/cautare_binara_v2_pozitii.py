e10fadcdabb773a7ff5522870e969505f7e5f501
Chirila Catalin-Dumitru <109412177+Chirila-Catalin@users.noreply.github.com>
2022-10-21 16:43:41 +0300

    Create cautare_binara_v2_pozitii.py

---
+++ laborator_02/cautare_binara_v2_pozitii.py
@@ -0,0 +1,40 @@
+from array import array
+
+lista=[]
+poz=[]
+numar_elemente=input("Numarul de elemente din lista: ")
+numar_elemente=int(numar_elemente)
+for i in range(0, int(numar_elemente)):
+    print("introdu elementul V[",i,"] =", end="")
+    elemente=input()
+    lista.append(elemente)
+print("lista=  ", lista)
+#lista.sort()
+
+
+def cautare_binara(lista, element, numar_elemente):
+    stanga=0
+    dreapta=len(lista)-1
+
+    while stanga<=dreapta:
+        mijloc=(stanga+dreapta)//2
+        if lista[mijloc]==element:
+            return mijloc
+            
+        elif lista[mijloc]<element:
+            stanga=mijloc+1
+        else:
+            dreapta=mijloc-1
+    return -1
+
+element=input("introdu numarul pe care vrei sa il cauti: ")
+
+
+
+print("numarul cautat se afla pe pozitia: ", cautare_binara(lista, element, numar_elemente))
+print("Numarul ", element, " apare de :", lista.count(element), " ori")
+
+for j in range(len(lista)):
+    if lista[j]==element:
+        poz.append(j)
+print("Numarul ", element, " apare pe pozitiile: ", poz)
