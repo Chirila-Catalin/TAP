f30dcd9d96a649430fddd1e6db01db0823494c7a
Chirila Catalin-Dumitru <109412177+Chirila-Catalin@users.noreply.github.com>
2022-10-21 16:47:44 +0300

    Create coduri_ascii.py

---
+++ laborator_03/coduri_ascii.py
@@ -0,0 +1,6 @@
+def ascii_code():
+    #Returneaza codul ASCII al caracterului introdus de utilizator
+    caracter = input("Introduceti un caracter: ")
+    ascii_value = ord(caracter)
+    print("Codul ASCII al caracterului introdus este: ", ascii_value)
+ascii_code()
